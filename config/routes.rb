dcb81b7955b72f2e8e1520fb9908da54f5c7ce33
ShoNakanishi <165697131+sho-R4i@users.noreply.github.com>
2024-05-18 16:13:40 +0000

    [Update]投稿フォームの作成(レイアウト未完成)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about', to: 'homes#about', as: :about
+  get 'homes/about', to: 'homes#about', as: 'about'
   resources :books
   resources :users, only: [:show, :edit, :update]
 end
