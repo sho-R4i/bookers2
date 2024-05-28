bb2da2c463b24d26d578ba420a05171f6b795c56
ShoNakanishi <165697131+sho-R4i@users.noreply.github.com>
2024-05-28 12:20:54 +0000

    [Finish]Bookers2開発終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about', to: 'homes#about', as: 'about'
+  get 'home/about', to: 'homes#about', as: 'about'
   resources :books
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :create, :index]
 end
