787e2355893ac134bc6ae2a13013a439a3b847e2
ShoNakanishi <165697131+sho-R4i@users.noreply.github.com>
2024-05-30 13:45:28 +0000

    [Finish]Bookers2開発終了

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,69 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = User.find(@book.user.id)
+  end
+
+  def edit
+    is_matching_login_user
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    is_matching_login_user
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = 'Book was successfully destroyed.'
+      redirect_to books_path
+    else
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
+end
